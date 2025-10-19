eb164e57cf27c954ada4d23d1b1ced7a376eac1c
dmitrined <dm.nedioglo@gmail.com>
2025-10-19 18:45:56 +0200

    ok

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,18 @@
+
+import type { JSX } from 'react'
+import './App.css'
+
+
+
+export default function App() : JSX.Element {
+
+  return (
+   <div> 
+    
+     TestPage 
+    
+   </div>
+  )
+}
+
+
